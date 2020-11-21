f5b4e4e1a37176e2f5b2bcf46b3d3ea1e5f5ec0c
MELI CANTAMUTTO <MELICANTAMUTTO@gmail.com>
2020-11-21 13:08:05 -0300

    ejercicio js

---
+++ app.js
@@ -0,0 +1,8 @@
+console.log('Celsius a Fahrenheit')
+
+alert('Celsius a Farhenheit')
+
+const celsius = prompt('Ingresa la cantidad de °Celsius');
+const fahrenheit = (Number(celsius) * 9/5) + 32;
+
+alert(`°${celsius} Celcius son °${fahrenheit} Fahrenheit`);
